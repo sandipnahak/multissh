5f29d5849ccaffd79cdfbd98e968ec1a3bff7d9c
Sandip Nahak <sandeep.cne@gmail.com>
2018-11-30 09:34:05 +0530

    Update solution1.sh

--- solution1.sh
+++ solution1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-time for host in `cat hosts.list`; do echo $host; ssh -q -o BatchMode=yes -o StrictHostKeyChecking=no $hostip   "uptime";echo $? ;done
+time for host in `cat hosts.list`; do echo $host; ssh -q -o BatchMode=yes -o StrictHostKeyChecking=no $host  "uptime";echo $? ;done
